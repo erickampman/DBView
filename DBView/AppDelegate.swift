1b389728acc2884203ec7a70d50c3b100f0ed924
Eric Kampman <erickampman@yahoo.com>
2023-02-19 08:23:02 -0800

    Initial Commit

---
+++ DBView/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  DBView
+//
+//  Created by Eric Kampman on 2/19/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+	func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+		return true
+	}
+
+
+}
+
---
+++ DBView/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DBView
+//
+//  Created by Eric Kampman on 2/19/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
